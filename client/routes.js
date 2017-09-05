3b2141f376b37db61ce0a0072d28f6c931b6923f
EricDav <alienyidavid4christ@gmail.com>
2017-09-05 11:01:46 +0100

    Update routes.js

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import HomePage from './components/homePage';
-import App from './components/App';
+import App from './components/app';
 import Dashboard from './components/dashboard/dashboard';
 import requireAuth from './utils/requireAuth';
 import requireAuthForHome from './utils/requireAuthForHomePage';
